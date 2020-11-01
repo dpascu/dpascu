31b7c3f2cb634818bb0ce0d0a45e0dd396f4bec8
dpascu <dany3pp@gmail.com>
2020-11-01 03:07:54 +0100

    page url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import Config from './config.type'
 
 const config: Config = {
-  host: process.env.PUBLIC_URL || '',
+  host: process.env.REACT_APP_URL || '',
   github: process.env.REACT_APP_GITHUB || '',
 }
 
